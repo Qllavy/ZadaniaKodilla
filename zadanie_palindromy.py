447c59935165dd942cac7c46f2817b25619f1dc9
Hubert Olech <h-olech@wp.pl>
2023-02-08 21:50:30 +0100

    Dodanie dokumentacji

--- zadanie_palindromy.py
+++ zadanie_palindromy.py
@@ -1,4 +1,7 @@
 def palindrom():
+    """
+    Returned true or false based on checking if the user supplied word is a palindrome.
+    """
     pal = str(input("Enter a word to check if it is a palindrome: "))
     if pal == pal[::-1]:
         return True
